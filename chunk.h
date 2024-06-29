015ca535a4ff70184441cd3065737ca7fa2aacc4
Trible69 <edwinamunoz357@gmail.com>
2024-06-29 14:41:27 -0500

    Memory Allocation

--- chunk.h
+++ chunk.h
@@ -4,5 +4,15 @@
 #include "common.h"
 typedef enum{
   OP_RETURN, 
-} OpCode; 
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code; 
+} Chunk;
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+
 #endif 
