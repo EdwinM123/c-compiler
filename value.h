2101b2e7f8deb2175a9374328dd4eb965acb46c0
Trible69 <edwinamunoz357@gmail.com>
2024-07-01 10:23:44 -0500

    instructions for constant memory array

--- value.h
+++ value.h
@@ -14,5 +14,8 @@ typedef struct{
 void initValueArray(ValueArray* array); 
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
+void printValue(Value value){
+    printf("%g", value);
+}
 
 #endif
