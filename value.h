b0d091827e8c1f28d5892b4b9d1a11d00be06a2e
Trible69 <edwinamunoz357@gmail.com>
2024-07-01 10:13:00 -0500

    Constant Values

--- value.h
+++ value.h
@@ -2,6 +2,17 @@
 #define vlox_value_h 
 #include "common.h"
 
-typedef double Value; 
+typedef double Value;
+
+typedef struct{
+    int count; 
+    int capacity;
+    Value* values;  
+
+} ValueArray; 
+
+void initValueArray(ValueArray* array); 
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
 
 #endif
