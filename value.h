9d16a7d9ffda897b4225ee25dbc740fd20770016
Trible69 <edwinamunoz357@gmail.com>
2024-07-02 23:10:41 -0500

    trying to fix bugs

--- value.h
+++ value.h
@@ -14,8 +14,6 @@ typedef struct{
 void initValueArray(ValueArray* array); 
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
-void printValue(Value value){
-    printf("%g", value);
-}
+void printValue(Value value);
 
 #endif
