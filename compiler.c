8a98e840c9a5f76a0c9ad24c387f5ff374c24fdd
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-05 11:09:55 -0500

    Precedence

--- compiler.c
+++ compiler.c
@@ -11,6 +11,20 @@ typedef struct{
   bool hadError; 
 } Parser; 
 
+typedef enum{
+  PREC_NONE, 
+  PREC_ASSIGNMENT, 
+  PREC_OR,
+  PREC_AND,
+  PREC_EQUALITY,
+  PREC_COMPARISON,
+  PREC_TERM, 
+  PREC_FACTOR,
+  PREC_UNARY,
+  PREC_CALL,
+  PREC_PRIMARY
+} Precedence;
+
 Parser parser; 
 Chunk* compilingChunk; 
 
@@ -105,8 +119,23 @@ static void number(){
   emitConstant(value);
 }
 
-static void expression(){
+static void unary(){
+  TokenType operatorType = parser.previous.type; 
 
+  parsePrecedence(PREC_UNARY);
+
+  switch(operatorType){
+    case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+    default: return;
+  }
+}
+
+static void parsePrecedence(Precedence precedence){
+
+}
+
+static void expression(){
+  parsePrecedence(PREC_ASSIGNMENT);
 }
 
 bool compile(const char* source, Chunk* chunk){
