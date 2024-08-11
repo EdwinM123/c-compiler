ce1641c286b25e682818af888725432976f9a025
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-11 18:23:24 -0500

    More Declarations

--- compiler.c
+++ compiler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -160,6 +161,11 @@ static void beginScope(){
 
 static void endScope(){
   current->scopeDepth--;
+
+  while(current->localCount>0 && current->locals[current->localCount-1].depth > current->scopeDepth){
+    emitByte(OP_POP);
+    current->localCount--;
+  }
 }
 
 static void expression();
@@ -170,6 +176,11 @@ static void parsePrecedence(Precedence precedence);
 
 static uint8_t identifierConstant(Token* name){
   return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static bool identifiersEqual(Token* a, Token* b){
+  if(a->length != b->length) return false;
+  return memcmp(a->start, b->start, a->length)==0;
 }
 
 static void addLocal(Token name){
@@ -187,6 +198,17 @@ static void declareVariable(){
   if(current->scopeDepth==0) return; 
 
   Token* name = &parser.previous;
+  for(int i=current->localCount-1; i>=0; i--){
+    Local* local = &current->locals[i];
+    if(local->depth!=-1&&local->depth < current->scopeDepth){
+      break;
+    }
+
+    if(identifersEqual(name, &local->name)){
+      error("Already a variable with this name in this scope.");
+    }
+  }
+
   addLocal(*name);
 }
 
