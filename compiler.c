75e0b913fcc388f87144462ddca2927b232c31b9
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-12 13:44:01 -0500

    Conditions

--- compiler.c
+++ compiler.c
@@ -492,11 +492,24 @@ static void forStatement(){
   }
 
   int loopStart = currentChunk()->count; 
-  consume(TOKEN_SEMICOLON, "Expect ';'.");
+  int exitJump=-1;
+  if(!match(TOKEN_SEMICOLON)){
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+    exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+  }
+
   consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
 
   statement();
   emitLoop(loopStart);
+
+  if(exitJump !=-1){
+    patchJump(exitJump);
+    emitByte(OP_POP);
+  }
   endScope();
 }
 
