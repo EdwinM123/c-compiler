beb5ea122a288a8b32af7a1c0bb12841cfa4e818
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-13 09:30:13 -0500

    Function Creation

--- object.c
+++ object.c
@@ -68,6 +68,10 @@ ObjString* copyString(const char* chars, int length){
 }
 
 static void printFunction(ObjFunction* function){
+  if (function->name==NULL){
+    print("<script>");
+    return;
+  }
   printf("<fn %s>", function->name->chars);
 }
 
