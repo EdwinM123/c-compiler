942c4360f27fa51aaba74bef032d119da283fc4c
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-18 12:43:45 -0500

    Unused Objects

--- memory.c
+++ memory.c
@@ -144,6 +144,29 @@ static void traceReferences(){
   }
 }
 
+static void sweep(){
+  Obj* previous = NULL;
+  Obj* object = vm.objects;
+  while(object !=NULL){
+    if(object->isMarked){
+      object->isMarked = false;
+      previous = object;
+      object = object->next;
+    }
+    else {
+      Obj* unreached = object;
+      object=object->next;
+      if(previous!=NULL){
+        previous->next=object;
+      }
+      else {
+        vm.objects = object;
+      }
+      freeOject(unreached);
+    }
+  }
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
@@ -151,6 +174,7 @@ void collectGarbage() {
 
   markRoots();
   traceReferences();
+  sweep();
 
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
