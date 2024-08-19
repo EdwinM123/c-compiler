44e1221c1acce2136638df3b76857007c0674eb9
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-19 18:31:41 -0500

    Invocations

--- compiler.c
+++ compiler.c
@@ -432,6 +432,11 @@ static void dot(bool canAssign){
     emitBytes(OP_SET_PROPERTY, name);
 
   }
+  else if (match(TOKEN_LEFT_PAREN)){
+    uint8_t argCount = argumentList();
+    emitBytes(OP_INVOKE, name);
+    emitByte(argCount);
+  }
   else {
     emitBytes(OP_GET_PROPERTY, name);
   }
