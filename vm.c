ba5c3ae29f821957c449b3ad464c703582748f3d
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-21 16:11:46 -0500

    Superclasses Error

--- vm.c
+++ vm.c
@@ -483,6 +483,11 @@ static InterpretResult run(){
         break;
       case OP_INHERIT: {
         Value superclass = peek(1);
+        if(!IS_CLASS(superclass)) {
+          runtimeError("Superclass must be a class.");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+
         ObjClass* subclass = AS_CLASS(peek(0));
         tableAndAll(&AS_CLASS(superclass)->methods, &subclass->methods);
         pop();
