f32b0a7adb1fe3c95c54039d7b5d391ba219625c
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-22 16:18:02 -0500

    true false nil

--- value.h
+++ value.h
@@ -12,10 +12,16 @@ typedef struct ObjString ObjString;
 
 #define QNAN ((uint64_t)0x7ffc000000000000)
 
+#define TAG_NIL   1 
+#define TAG_FALSE 2
+#define TAG_TRUE  3
+
 typedef uint64_t Value;
 
+#define IS_NIL(value)    ((value)==NIL_VAL)
 #define IS_NUMBER(value) (((value)&QNAN) != QNAN)
 
+#define AS_BOOL(value)   ((value)==TRUE_VAL)
 #define AS_NUMBER(value) valueToNum(value)
 
 static inline double valueToNum(Value value){
@@ -24,6 +30,10 @@ static inline double valueToNum(Value value){
   return num;
 }
 
+#define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL   ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL    ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL     ((Value)(uint64_t)(QNAN | TAG_NIL))
 #define NUMBER_VAL(num) numToValue(num)
 
 static inline Value numToValue(double num) {
