743334b1f61316ed72ee5c1a1d318380a28d4598
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-24 18:47:22 -0500

    Abstract Syntax

--- defs.h
+++ defs.h
@@ -11,3 +11,14 @@ struct token {
     int token;
     int intvalue;
 };
+
+enum {
+    A_ADD, A_SUBTRACT, A_MULTIPLY, A_DIVIDE, A_INTLIT
+};
+
+struct ASTnode {
+    int op;
+    struct ASTnode *left; 
+    struct ASTnode *right;
+    int intvalue;
+};
