8da68ff7e57fc03d561b8219c5a37ae4e38c22a4
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-28 21:37:58 -0500

    Revert "Functions in scan.c"
    This reverts commit 992268e74e6285446882046deb2863bd24ef7013.

--- defs.h
+++ defs.h
@@ -1,13 +1,8 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
-enum {
-    T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT
-};
-
 struct token {
     int token;
     int intvalue;
+};
+
+enum {
+    T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT
 };
